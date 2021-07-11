cd17ae67958cd954558249b55c751e1adc1dbf2e
NitichaiSawangsai <40546651+NitichaiSawangsai@users.noreply.github.com>
2021-07-11 14:17:10 +0700

    Feature/chatbot (#22)
    * feat: clear page
    
    * feat: install nuxt-i18n

---
+++ components/ui/ChatBot/ChatBot.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="chatbot">
+    <v-tooltip left>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          fab
+          fixed
+          medium
+          bottom
+          right
+          color="primary"
+          class="chat-btn"
+          v-bind="attrs"
+          link
+          target="_blank"
+          v-on="on"
+        >
+          <v-icon class="position-icon" x-large color="white" v-text="'mdi-chat-processing-outline'" />
+        </v-btn>
+      </template>
+      <span>{{ $t('CHAT_BOT.CHAT_HELLO') }}</span>
+    </v-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatBot'
+}
+</script>
