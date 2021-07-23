963ca2cb5bbbba0b8881c9d564bed63821483e15
NitichaiSawangsai <40546651+NitichaiSawangsai@users.noreply.github.com>
2021-07-24 00:18:31 +0700

    Feature/sco web page (#63)
    * fix: text alert
    
    * fix: update code seo
    
    * fix: update code seo
    
    * fix: update code seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
+import textSEO from './utils/textSEO'
 
 export default {
   target: 'static',
@@ -18,18 +19,60 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Nitichai Sawangsai',
-    title: 'Nitichai Sawangsai',
+    titleTemplate: `%s | ${textSEO.meta.nameTopic}`,
+    title: textSEO.meta.nameTopic,
     htmlAttrs: {
-      lang: 'en'
+      lang: textSEO.meta.lang
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nitichai Sawangsai' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: textSEO.meta.charset },
+
+      { name: 'viewport', content: textSEO.meta.viewport },
+      { name: 'robots', content: textSEO.meta.robots },
+      { name: 'format-detection', content: textSEO.meta['format-detection'] },
+      {
+        name: 'keywords',
+        content: textSEO.meta.keywords
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: textSEO.meta.description
+      },
+      {
+        property: 'og:title',
+        name: 'og:title',
+        content: textSEO.meta.nameTopic
+      },
+      {
+        property: 'og:description',
+        name: 'og:description',
+        content: textSEO.meta.description
+      },
+      {
+        property: 'og:url', name: 'og:url', content: textSEO.meta['og:url']
+      },
+      {
+        property: 'og:site_name', name: 'og:site_name', content: textSEO.meta['og:site_name']
+      },
+      {
+        property: 'og:image',
+        name: 'og:image',
+        content: textSEO.meta['og:image']
+      },
+      { property: 'og:type', name: 'og:type', content: textSEO.meta['og:type'] },
+      { property: 'facebook:title', name: 'facebook:title', content: textSEO.meta['facebook:title'] },
+      { property: 'facebook:image', name: 'facebook:image', content: textSEO.meta['facebook:image'] },
+      { property: 'linkedin:title', name: 'linkedin:title', content: textSEO.meta['linkedin:title'] },
+      { property: 'linkedin:image', name: 'linkedin:image', content: textSEO.meta['linkedin:image'] },
+      { property: 'twitter:card', name: 'twitter:card', content: textSEO.meta['twitter:card'] }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: textSEO.link.icon },
+      {
+        rel: 'canonical',
+        href: textSEO.link.canonical
+      }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
--- plugins/head-util.js
+++ plugins/head-util.js
@@ -1,49 +1,54 @@
+import textSEO from './../utils/textSEO'
 export default ({ app }, inject) => {
   inject('headUtil', data => ({
+    titleTemplate: data.titleTemplate || `%s | ${textSEO.meta.nameTopic}`,
+    title: data.title || textSEO.meta.nameTopic,
     htmlAttrs: {
-      lang: 'en'
+      lang: data.htmlAttrs.lang || textSEO.meta.lang
     },
-    title: data.title,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { charset: data.charset || textSEO.meta.charset },
+      { name: 'viewport', content: data.viewport || textSEO.meta.viewport },
+      { name: 'robots', content: data.robots || textSEO.meta.robots },
+      { name: 'format-detection', content: data['format-detection'] || textSEO.meta['format-detection'] },
       {
         hid: 'description',
         name: 'description',
-        content: data.description || 'แสดงผลงานของนายนิธิชัย แสวงสาย ชื่อเล่นออม เรียนจบสาขาวิศวกรรมซอฟต์แวร์จากมหาวิทยาลัยบูรพา สหกิจศึกษาที่ บริษัท บิทคับ บล็อคเชน เทคโนโลยี จำกัด จบมาทำงานที่บริษัท Globish หลังจาก 1 ปี ย้ายมาทำงานที่บริษัท   รับทําเว็บไซต์ เว็บแอปพลิเคชัน'
+        content: data.description || textSEO.meta.description
       },
       {
         name: 'keywords',
-        content: ['Nitichai', 'Sawangsai', 'Nitichai Sawangsai', 'nitichaisawangsai', 'NitichaiSawangsai', 'รับทำเว็บ', 'สร้างเว็บ']
+        content: data.keywords || textSEO.meta.keywords
       },
       {
         property: 'og:title',
         name: 'og:title',
-        content: data.title
+        content: data.title || textSEO.meta.nameTopic
       },
       {
         property: 'og:description',
         name: 'og:description',
-        content: data.description
+        content: data.description || textSEO.meta.description
       },
       {
-        property: 'og:url', name: 'og:url', content: 'nitichai.vercel.app'
+        property: 'og:url', name: 'og:url', content: textSEO.meta['og:url']
       },
       {
-        property: 'og:site_name', name: 'og:site_name', content: 'nitichai'
+        property: 'og:site_name', name: 'og:site_name', content: textSEO.meta['og:site_name']
       },
       {
         property: 'og:image',
         name: 'og:image',
-        content: data.image
-      }
+        content: textSEO.meta['og:image']
+      },
+      { property: 'og:type', name: 'og:type', content: textSEO.meta['og:type'] }
     ],
     link: [
       {
         rel: 'canonical',
-        href: data.urlPath ? data.urlPath : 'https://nitichai.vercel.app'
-      }
+        href: data.urlPath ? data.urlPath : textSEO.link.canonical
+      },
+      { rel: 'icon', type: 'image/x-icon', href: data.icon || textSEO.link.icon }
     ]
   }))
 }
