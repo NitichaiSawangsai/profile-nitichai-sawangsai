a98529542287796f7f51ad35ca04fc035f7b6b1e
NitichaiAom <NitichaiSawangsai@gmail.com>
2021-07-23 23:15:09 +0700

    fix: update code seo

--- plugins/head-util.js
+++ plugins/head-util.js
@@ -1,3 +1,4 @@
+const textDescription = 'แสดงผลงานของนายนิธิชัย แสวงสาย ชื่อเล่นออม เรียนจบสาขาวิศวกรรมซอฟต์แวร์จากมหาวิทยาลัยบูรพา สหกิจศึกษาที่ บริษัท บิทคับ บล็อคเชน เทคโนโลยี จำกัด จบมาทำงานที่บริษัท Globish หลังจาก 1 ปี ย้ายมาทำงานที่บริษัท SCG Digital Office (SCG Cement - Building Materials) ตำแหน่ง Associate Technology Developer และตอนนี้รับทําเว็บไซต์ และเว็บแอปพลิเคชัน ด้วยตัวเอง'
 export default ({ app }, inject) => {
   inject('headUtil', data => ({
     htmlAttrs: {
@@ -6,26 +7,28 @@ export default ({ app }, inject) => {
     title: data.title,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
+      { name: 'robots', content: 'index,follow' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'title', content: 'รับทำเว็บไซต์ เว็บแอปพลิเคชัน | Nitichai Sawangsai' },
       {
         hid: 'description',
         name: 'description',
-        content: data.description || 'แสดงผลงานของนายนิธิชัย แสวงสาย ชื่อเล่นออม เรียนจบสาขาวิศวกรรมซอฟต์แวร์จากมหาวิทยาลัยบูรพา สหกิจศึกษาที่ บริษัท บิทคับ บล็อคเชน เทคโนโลยี จำกัด จบมาทำงานที่บริษัท Globish หลังจาก 1 ปี ย้ายมาทำงานที่บริษัท   รับทําเว็บไซต์ เว็บแอปพลิเคชัน'
+        content: data.description || textDescription
       },
       {
         name: 'keywords',
-        content: ['Nitichai', 'Sawangsai', 'Nitichai Sawangsai', 'nitichaisawangsai', 'NitichaiSawangsai', 'รับทำเว็บ', 'สร้างเว็บ']
+        content: ['Nitichai', 'Sawangsai', 'Nitichai Sawangsai', 'nitichaisawangsai', 'NitichaiSawangsai', 'รับทำเว็บ', 'สร้างเว็บ', 'เว็บแอปพลิเคชัน', 'เว็บไซต์', 'website', 'web application', 'วิศวกรรมซอฟต์แวร์', 'Software Engineering']
       },
       {
         property: 'og:title',
         name: 'og:title',
-        content: data.title
+        content: data.title || 'Nitichai Sawangsai'
       },
       {
         property: 'og:description',
         name: 'og:description',
-        content: data.description
+        content: data.description || textDescription
       },
       {
         property: 'og:url', name: 'og:url', content: 'nitichai.vercel.app'
@@ -36,14 +39,15 @@ export default ({ app }, inject) => {
       {
         property: 'og:image',
         name: 'og:image',
-        content: data.image
+        content: data.image || 'https://avatars.githubusercontent.com/u/40546651?v=4'
       }
     ],
     link: [
       {
         rel: 'canonical',
         href: data.urlPath ? data.urlPath : 'https://nitichai.vercel.app'
-      }
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   }))
 }
