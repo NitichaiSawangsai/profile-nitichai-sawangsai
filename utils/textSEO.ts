19f82ae84ffdd47e50cddbcb228450237d16dc60
NitichaiAom <NitichaiSawangsai@gmail.com>
2021-07-24 17:57:22 +0700

    feat: fixed logo

--- utils/textSEO.ts
+++ utils/textSEO.ts
@@ -2,11 +2,11 @@ export default {
   meta: {
     charset: 'utf-8',
     lang: 'en',
-    nameTopic: 'แสดงผลงาน | Nitichai Sawangsai',
+    nameTopic: 'Nitichai Sawangsai',
     viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
     robots: 'index,follow',
     'format-detection': 'telephone=no',
-    title: 'แสดงผลงาน | Nitichai Sawangsai',
+    title: 'หน้าแรก',
     description: 'แสดงผลงานของนายนิธิชัย แสวงสาย ชื่อเล่นออม เรียนจบสาขาวิศวกรรมซอฟต์แวร์ มหาวิทยาลัยบูรพา สหกิจศึกษาที่ บริษัท บิทคับ บล็อคเชน เทคโนโลยี จำกัด จบมาทำงานที่บริษัท Globish Academia หลังจากทำงานมาสักระยะหนึ่ง ย้ายมาทำงานที่บริษัท SCG Digital Office (SCG Cement - Building Materials) ตำแหน่ง Associate Technology Developer',
     keywords: ['Nitichai', 'Sawangsai', 'Nitichai Sawangsai', 'nitichaisawangsai', 'NitichaiSawangsai', 'รับทำเว็บ', 'สร้างเว็บ', 'เว็บแอปพลิเคชัน', 'เว็บไซต์', 'website', 'web application', 'วิศวกรรมซอฟต์แวร์', 'Software Engineering'],
     'og:title': 'Nitichai Sawangsai',
